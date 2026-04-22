b3e52859421dd76db62706f141f4e1d27788fcb9
dte-84 <dte.solutions.llc@gmail.com>
2026-04-22 11:55:48 -0500

    Sync pnpm-lock.yaml with package.json

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig(({ mode }) => ({
   },
   build: {
     outDir: "dist",
+    sourcemap: true, // Enable source maps for production build
   },
   plugins: [react(), expressPlugin()],
   resolve: {
