78656abcdf50c4d448e700cb357416bc014ab622
dte-84 <dte.solutions.llc@gmail.com>
2026-04-24 17:05:11 -0500

    Sync pnpm-lock.yaml with package.json

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import shibainuImg from "/public/shibainu.png";
+import shibainuImg from "/shibainu.png";
 import { CheckCircle2, Truck, Lock, RotateCcw, Star } from "lucide-react";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
